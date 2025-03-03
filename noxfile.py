285406874d3a95c5fda6dcfdf66c0f776d1fb139
Luís Miranda <15702603+luistm@users.noreply.github.com>
2025-03-03 20:43:21 +0000

    Drop python 3.10 and welcome python 3.13

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.10", "3.11", "3.12"])
+@nox.session(python=["3.12", "3.13"])
 def tests(session):
     session.install("-r", "requirements.txt")
     session.run("pytest", ".")
